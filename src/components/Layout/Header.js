5aacaf96da05906a98d6ee1cf321d478df04ccf2
iamASR27 <ramardeep1997@gmail.com>
2023-08-04 20:24:30 +0530

    started foodOrderApp

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import styles from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Your Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} alt="meals" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
