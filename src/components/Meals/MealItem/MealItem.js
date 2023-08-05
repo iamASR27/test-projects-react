0efd2822d7489602ad2a2e22ceeacde43ce3b3ab
iamASR27 <ramardeep1997@gmail.com>
2023-08-05 19:10:11 +0530

    added add button and input

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import styles from './MealItem.module.css';
+import React from "react";
+import MealItemForm from "./MealItemForm";
+import styles from "./MealItem.module.css";
 
-const MealItem = props => {
-    const price = `$${props.price.toFixed(2)}`;
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
 
-    return <li className={styles.meal}>
-        <div><h3>{props.name}</h3>
+  return (
+    <li className={styles.meal}>
+      <div>
+        <h3>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
-        </div>
-        <div>
-
-        </div>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
-}
+  );
+};
 
 export default MealItem;
